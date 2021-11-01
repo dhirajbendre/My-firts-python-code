f3410683be117dbc031497571d3f2d4c805026ad
dhirajbendre <75013590+dhirajbendre@users.noreply.github.com>
2021-11-01 17:56:44 +0530

    Add files via upload

---
+++ 972nd.py
@@ -0,0 +1,7 @@
+pocketmoney = int(input("Enter the pocket money you get:"))
+if(pocketmoney>500):
+    print("U are a rich kid")    
+elif(pocketmoney>100):
+    print("U have a good life")
+else:
+    print("i know what you feel")            
